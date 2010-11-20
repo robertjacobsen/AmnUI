5a91f5330df622dc5fce4496da68e55709f3a393
Robert Jacobsen <robert@robertjacobsen.net>
2010-11-20 13:14:26 +0100

    Fixed chatframe weirdness when resizing.

--- frames.lua
+++ frames.lua
@@ -1,8 +1,13 @@
 local panel = CreateFrame("BUTTON", nil, UIParent)
---[[
- Function FixChat
--- A function that removes the textures from the ChatFrames.
---]]
+
+-- Define configuration
+panel.sizes = { width = 450, 
+		height = 280, }
+
+panel.colors = { tBlack = {r = 0, g = 0, b = 0, a = 0.7},
+		 sBlack = {r = 0, g = 0, b = 0, a = 1},
+		 tBlue  = {r = 0.243, g = 0.298, b = 0.345, a = 0.7}, }
+
 local FixChat = function()
         for i = 1,7 do
                 for k,v in pairs(CHAT_FRAME_TEXTURES) do
@@ -13,20 +18,17 @@ local FixChat = function()
         for k in pairs(CHAT_FRAME_TEXTURES) do
                 CHAT_FRAME_TEXTURES[k] = nil
         end
+
+        ChatFrame1:SetWidth(panel.sizes.width)
+        ChatFrame1:ClearAllPoints()
+	ChatFrame1:SetUserPlaced(true)
+        ChatFrame1:SetPoint("BOTTOM", panel.chatframe, 0, 3)
+        ChatFrame1:SetPoint("TOP", panel.chatframe, 0, -3)
+        ChatFrame1:SetPoint("LEFT", panel.chatframe, 3, 0)
+        ChatFrame1:SetPoint("RIGHT", panel.chatframe, -3, 0)
+        FCF_SetLocked(ChatFrame1, 1)
 end
 
---[[
- Function MakeFrame 
--- The function that makes a frame with the given options taken from the arguments
--- Parameters:
--- name - Name of the frame
--- width - The width of the frame
--- height - The height of the frame
--- r - Amount of red in the colour of the frame
--- g - Amount of green in the colour of the frame
--- b - Amount of blue in the colour of the frame
--- a - Alpha (transparency) of the frame
---]]
 local MakeFrame = function(width, height, c) 
         local frame = CreateFrame("Frame", nil, UIParent)
 
@@ -43,41 +45,31 @@ local MakeFrame = function(width, height, c)
         return frame
 end
 
-panel.sizes = { width = 450, height = 280 }
+panel:RegisterEvent"PLAYER_LOGIN"
+panel:RegisterEvent"UI_SCALE_CHANGED"
+panel:RegisterEvent"DISPLAY_SIZE_CHANGED"
 
-panel:RegisterEvent("PLAYER_LOGIN")
-panel:SetScript("OnEvent", function(self) 
-        --[[ The left end side of the screen, chat frames etc ]]
-		
-		local colors = {
-			tBlack = {r = 0, g = 0, b = 0, a = 0.7},
-			sBlack = {r = 0, g = 0, b = 0, a = 1},
-			tBlue = {r = 0.243, g = 0.298, b = 0.345, a = 0.7},
-		}
+panel:SetScript("OnEvent", function(self, event, ...) 
+	self[event](...)
+end)
 
-
-        local cf = MakeFrame(panel.sizes.width, panel.sizes.height, colors.tBlack) 
-        cf:SetPoint("BOTTOMLEFT", UIParent, "BOTTOMLEFT", 45, 50)
+panel.UI_SCALE_CHANGED = FixChat
+panel.DISPLAY_SIZE_CHANGED = FixChat
+panel.PLAYER_LOGIN = function() 
+	--[[ The left end side of the screen, chat frames etc ]]
+        panel.chatframe = MakeFrame(panel.sizes.width, panel.sizes.height, panel.colors.tBlack) 
+        panel.chatframe:SetPoint("BOTTOMLEFT", UIParent, "BOTTOMLEFT", 45, 50)
         FixChat() -- We don't like textures!
 
-        local cful = MakeFrame(2, panel.sizes.height + 2, colors.tBlue) 
-        cful:SetPoint("RIGHT", cf, "LEFT")
+        local cful = MakeFrame(2, panel.sizes.height + 2, panel.colors.tBlue) 
+        cful:SetPoint("RIGHT", panel.chatframe, "LEFT")
 
-        local cfur = MakeFrame(2, panel.sizes.height + 2, colors.tBlue) 
-        cfur:SetPoint("LEFT", cf, "RIGHT")
+        local cfur = MakeFrame(2, panel.sizes.height + 2, panel.colors.tBlue) 
+        cfur:SetPoint("LEFT", panel.chatframe, "RIGHT")
 
-        local cfut = MakeFrame(panel.sizes.width + 4, 2, colors.tBlue) 
-        cfut:SetPoint("BOTTOM", cf, "TOP")
+        local cfut = MakeFrame(panel.sizes.width + 4, 2, panel.colors.tBlue) 
+        cfut:SetPoint("BOTTOM", panel.chatframe, "TOP")
 
-        local cfub = MakeFrame(panel.sizes.width + 4, 2, colors.tBlue) 
-        cfub:SetPoint("TOP", cf, "BOTTOM")
-        
-        ChatFrame1:SetWidth(panel.sizes.width)
-        ChatFrame1:ClearAllPoints()
-		ChatFrame1:SetUserPlaced(true)
-        ChatFrame1:SetPoint("BOTTOM", cf, 0, 3)
-        ChatFrame1:SetPoint("TOP", cf, 0, -3)
-        ChatFrame1:SetPoint("LEFT", cf, 3, 0)
-        ChatFrame1:SetPoint("RIGHT", cf, -3, 0)
-        FCF_SetLocked(ChatFrame1, 1)
-end)
+        local cfub = MakeFrame(panel.sizes.width + 4, 2, panel.colors.tBlue) 
+        cfub:SetPoint("TOP", panel.chatframe, "BOTTOM")
+end
